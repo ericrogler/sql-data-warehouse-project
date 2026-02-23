fc735c97d0a5e959c3b3cc2d0e2e782b6ce20b6f
Eric Rogler <ericjrogler@gmail.com>
2026-02-23 15:07:20 -0700

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -10,24 +10,31 @@ THIS SCRIPT WILL PERMANENTLY DELETE EXISTING DATA IF AN EXISTING
 */
 
 USE master;
-GO -- These are separators when executing code
+GO
 
--- Check if it exists.
+-- Drop database if it already exists
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
-	ALTER DATABASE DataWarehouse SET SINGLE USER WITH ROLLBACK IMMEDIATE;
+    ALTER DATABASE DataWarehouse 
+        SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 
--- Create initial database
+    DROP DATABASE DataWarehouse;
+END
+GO
+
+-- Create fresh database
 CREATE DATABASE DataWarehouse;
 GO
 
 USE DataWarehouse;
 GO
 
--- Create Schemas
+-- Create schemas
 CREATE SCHEMA bronze;
-GO 
+GO
+
 CREATE SCHEMA silver;
 GO
+
 CREATE SCHEMA gold;
 GO
